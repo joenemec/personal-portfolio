608599f4234945c60d65fba7367cf77e425cf147
Joseph Nemec <jnemec84@gmail.com>
2022-10-15 19:13:10 -0500

    add header js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+// NAVBAR TOGGLE
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+};
+
+addEventOnElem(navToggler, "click", toggleNav);
