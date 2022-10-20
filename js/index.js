f44fce05e537b3c54dab5e0e85d0efd458f91b3c
Joseph Nemec <jnemec84@gmail.com>
2022-10-20 16:57:14 -0500

    redo skills section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 "use strict";
 
+// LOADING SCREEN
+const loadScreen = document.querySelector(".loading-screen");
+const body = document.body;
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+  setTimeout(() => {
+    loadScreen.classList.toggle("inactive");
+    body.style.overflowY = "unset";
+  }, 1800);
+};
+
+// window.onbeforeunload();
+
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
@@ -23,3 +37,13 @@ const toggleNav = function () {
 };
 
 addEventOnElem(menuContent, "click", toggleNav);
+
+// CONTACT TOGGLE
+const contact = document.querySelector("[data-contact]");
+const contactContent = document.querySelectorAll("[data-contact-toggler]");
+
+const toggleContact = function () {
+  contact.classList.toggle("active");
+};
+
+addEventOnElem(contactContent, "click", toggleContact);
