7901f19ea89e858887f48b60744a490c4c28a51e
Joseph Nemec <jnemec84@gmail.com>
2022-10-21 10:12:08 -0500

    add lazy load images

--- js/index.js
+++ js/index.js
@@ -14,6 +14,33 @@ window.onbeforeunload = function () {
 
 window.onbeforeunload();
 
+// LAZY LOADING IMAGES
+const imgTargets = document.querySelectorAll("img[data-src]");
+console.log(imgTargets);
+
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", () => {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+// CLICK EVENTS
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
